41ae77080139b311e5f8c09e6cabfc217fa9c455
raspberry-pi <raspberry-pi@kakao.com>
2023-06-14 20:02:15 +0900

    test case1, case2, matmul

--- hw2/part2/file_generator.py
+++ hw2/part2/file_generator.py
@@ -2,7 +2,7 @@ import os
 
 file_size = 1024 * 1024  # 1MB in bytes
 
-dst_path = 'ach'
+dst_path = 'src-directory'
 for i in range(1, 101):
     file_name = os.path.join(dst_path,f"file_{i}.txt")
     with open(file_name, "wb") as file:
