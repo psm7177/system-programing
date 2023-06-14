80e3c05d4d947925c6bfaad4c0f28db3e4b6e8c5
raspberry-pi <raspberry-pi@kakao.com>
2023-06-14 10:39:31 +0000

    Implement file generator

---
+++ hw2/part2/file_generator.py
@@ -0,0 +1,10 @@
+import os
+
+file_size = 1024 * 1024  # 1MB in bytes
+
+dst_path = 'ach'
+for i in range(1, 101):
+    file_name = os.path.join(dst_path,f"file_{i}.txt")
+    with open(file_name, "wb") as file:
+        file.write(os.urandom(file_size))
+    print(f"Created {file_name}")
